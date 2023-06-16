c8fd359e2fad6604f5955221554ed6536db37a12
Igor Mishur <igor.mishur@gmail.com>
2023-06-16 10:30:15 +0300

    some stubs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,19 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub struct EmailVerifier {
+    dns_server: String
+}
+
+pub struct VerificationResult {
+    mx: MxResult
+}
+
+struct MxResult {
+    accepts_mail: bool,
+    mx_records: Vec<String>
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
