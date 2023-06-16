e945b6a8fed7def769dd4bce50904a90dea68920
Igor Mishur <igor.mishur@gmail.com>
2023-06-16 10:59:06 +0300

    initial stubs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 pub struct EmailVerifier {
     dns_server: String
 }
@@ -15,13 +11,25 @@ struct MxResult {
     mx_records: Vec<String>
 }
 
+impl EmailVerifier {
+    pub fn new(dns_server:&'static str) -> Self {
+        Self {dns_server:String::from(dns_server)}
+    }
+    pub fn verify_static(email:&'static str) -> VerificationResult{
+        Self::verify(String::from(email))
+    }
+    pub fn verify(email:String)-> VerificationResult{
+        VerificationResult{mx:MxResult{accepts_mail:false, mx_records:Vec::<String>::new()}}
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn dns_is_in_place() {
+        let result = EmailVerifier::new("8.8.8.8:53");
+        assert_eq!(result.dns_server, String::from("8.8.8.8:53"));
     }
 }
